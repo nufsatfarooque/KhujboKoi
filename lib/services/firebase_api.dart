daab3bc598ab6d0c78a6c59924a493a3d29a5e5d
AhmedRafid3S5 <ahmedrafidx360@gmail.com>
2024-11-21 13:34:56 +0600

    added firebase_messaging to dependencies for push notification

---
+++ lib/services/firebase_api.dart
@@ -0,0 +1,10 @@
+class FirebaseApi {
+  //create an instance of Firebase Messaging
+
+  //function to initialize notifications
+
+  //function to handle received messages
+
+  //function to initialize foreground and background settings
+  
+}
